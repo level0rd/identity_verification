2f01e22ba710497e52ab35b46b93d226b3abc5c4
level0rd <45522296+level0rd@users.noreply.github.com>
2024-03-08 19:36:08 +0300

    Update person_identity.py
    Adding comments

--- person_identity.py
+++ person_identity.py
@@ -11,6 +11,12 @@ import streamlit as st
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
 PADDING = 50
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+THRESHOLD = 0.60
+RECTANGLE_THICKNESS = 2
+FACE_THICKNESS = 1
 
 # Models loading
 shape_predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks_GTX.dat')
@@ -20,20 +26,25 @@ detector = dlib.get_frontal_face_detector()
 streaming_placeholder = st.empty()
 
 
-class CamImageLoad:
+class CameraImageLoad:
     def __init__(self):
         self.check_button_pressed = False
         self.image_cam = None
 
 
-cam_image_load = CamImageLoad()
+camera_image_load = CameraImageLoad()
 
 
-def shape_to_np(shape, dtype="int"):
+def shape_to_np(shape: dlib.full_object_detection, dtype: str = "int") -> np.ndarray:
     """
+    Convert shape to a numpy array.
 
-    Convert shape to numpy array.
+    Args:
+        shape: The shape object to be converted.
+        dtype (str): Data type of the numpy array.
 
+    Returns:
+        np.ndarray: Converted numpy array.
     """
 
     cord = np.zeros((shape.num_parts, 2), dtype=dtype)
@@ -42,77 +53,92 @@ def shape_to_np(shape, dtype="int"):
     return cord
 
 
-def get_descriptor(img):
+def get_descriptor(image: np.ndarray) -> tuple:
+    """
+    Compute face descriptor.
+
+    Args:
+        image: Image data.
+
+    Returns:
+        tuple: Bounding box and face descriptor.
     """
 
-    Desiccator calculation.
+    bbox, shape = get_shape(image)
+    return bbox, face_recognition.compute_face_descriptor(image, shape)
 
+
+def get_shape(image: np.ndarray) -> tuple:
+    """
+    Obtain key points and bounding boxes.
+
+    Args:
+        image: Image data.
+
+    Returns:
+        tuple: Bounding box and shape object.
     """
 
-    bbox, shape = get_shape(img)
-    return bbox, face_recognition.compute_face_descriptor(img, shape)
+    dets = detector(image, 1)
 
+    for face_index, face_rectangle in enumerate(dets):
+        shape = shape_predictor(image, face_rectangle)
 
-def get_shape(img):
-    """
-
-    Obtaining key points and bounding boxes.
-
-    """
-
-    dets = detector(img, 1)
-
-    for k, d in enumerate(dets):
-        shape = shape_predictor(img, d)
-
-        numbers = re.findall(r'\d+', str(d))
+        numbers = re.findall(r'\d+', str(face_rectangle))
         temp_str = ', '.join(numbers)
         bbox = temp_str.split(',')
 
     return bbox, shape
 
 
-def draw_facemask(face, bbox, shape, bbox_color):
+def draw_facemask(face: np.ndarray, bbox: list, shape: dlib.full_object_detection, bbox_color: tuple) -> np.ndarray: 
     """
+    Draw facemask on the face.
 
-    Drawing key points and limiting frames.
+    Args:
+        face: Input face image.
+        bbox: Bounding box coordinates.
+        shape: Shape object representing face landmarks.
+        bbox_color: Color for bounding box.
 
+    Returns:
+        np.ndarray: Modified face image with facemask.
     """
 
     points = shape_to_np(shape)
 
-    cv2.rectangle(face, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), bbox_color, 2)
+    cv2.(face, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), bbox_color, RECTANGLE_THICKNESS)
 
     # Lower border of the face
     for i in range(16):
-        cv2.line(face, points[i], points[i + 1], (0, 0, 255), 1)
+        cv2.line(face, points[i], points[i + 1], BLUE, FACE_THICKNESS)
 
     # Lips
     for i in range(48, 60):
-        cv2.line(face, points[i], points[i + 1], (0, 0, 255), 1)
+        cv2.line(face, points[i], points[i + 1], BLUE, FACE_THICKNESS)
 
     # Nose
     for i in range(27, 35):
-        cv2.line(face, points[i], points[i + 1], (0, 0, 255), 1)
-    cv2.line(face, points[35], points[30], (0, 0, 255), 1)
+        cv2.line(face, points[i], points[i + 1], BLUE, FACE_THICKNESS)
+    cv2.line(face, points[35], points[30], BLUE, FACE_THICKNESS)
 
     # Right eye
     for i in range(36, 41):
-        cv2.line(face, points[i], points[i + 1], (0, 0, 255), 1)
-    cv2.line(face, points[36], points[41], (0, 0, 255), 1)
+        cv2.line(face, points[i], points[i + 1], BLUE, FACE_THICKNESS)
+    cv2.line(face, points[36], points[41], BLUE, FACE_THICKNESS)
 
     # Left eye
     for i in range(42, 47):
-        cv2.line(face, points[i], points[i + 1], (0, 0, 255), 1)
-    cv2.line(face, points[42], points[47], (0, 0, 255), 1)
+        cv2.line(face, points[i], points[i + 1], BLUE, FACE_THICKNESS)
+    cv2.line(face, points[42], points[47], BLUE, FACE_THICKNESS)
 
     # Right brow
     for i in range(17, 21):
-        cv2.line(face, points[i], points[i + 1], (0, 0, 255), 1)
+        cv2.line(face, points[i], points[i + 1], BLUE, FACE_THICKNESS)
 
     # Left brow
     for i in range(22, 26):
-        cv2.line(face, points[i], points[i + 1], (0, 0, 255), 1)
+        cv2.line(face, points[i], points[i + 1], BLUE, FACE_THICKNESS)
 
     x1 = max(0, int(bbox[0]) - PADDING)
     y1 = max(0, int(bbox[1]) - PADDING)
@@ -123,14 +149,22 @@ def draw_facemask(face, bbox, shape, bbox_color):
 
 
 def callback(frame: av.VideoFrame) -> av.VideoFrame:
+    """
+    Callback function for processing video frames.
 
-    img_rgb = frame.to_ndarray(format="rgb24")
+    Args:
+        frame: Input video frame.
 
-    if cam_image_load.check_button_pressed:
-        cam_image_load.image = img_rgb
-        cam_image_load.check_button_pressed = False
+    Returns:
+        av.VideoFrame: Processed video frame.
+    """
+    image_rgb = frame.to_ndarray(format="rgb24")
 
-    return av.VideoFrame.from_ndarray(img_rgb, format="rgb24")
+    if camera_image_load.check_button_pressed:
+        camera_image_load.image = image_rgb
+        camera_image_load.check_button_pressed = False
+
+    return av.VideoFrame.from_ndarray(image_rgb, format="rgb24")
 
 
 with streaming_placeholder.container():
@@ -143,56 +177,56 @@ with streaming_placeholder.container():
         async_processing=True,
     )
 
-photo_doc = st.file_uploader("Upload the photo from the document")
+photo_document = st.file_uploader("Upload the photo from the document")
 
 if st.button('Confirm'):
 
-    cam_image_load.check_button_pressed = True
+    camera_image_load.check_button_pressed = True
 
-    if photo_doc is not None:
-        image_array = np.frombuffer(photo_doc.read(), dtype=np.uint8)
-        image_doc = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-        image_doc = cv2.cvtColor(image_doc, cv2.COLOR_BGR2RGB)
+    if photo_document is not None:
+        image_array = np.frombuffer(photo_document.read(), dtype=np.uint8)
+        image_document = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        image_document = cv2.cvtColor(image_document, cv2.COLOR_BGR2RGB)
 
-        col_left, col_right = st.columns([1, 1])
+        left_column, right_column = st.columns([1, 1])
 
-        bbox_doc, shape_doc = get_shape(image_doc)
-        bbox_cam, shape_cam = get_shape(cam_image_load.image)
+        bbox_document, shape_document = get_shape(image_document)
+        bbox_camera, shape_camera = get_shape(camera_image_load.image)
 
-        image_cam = cam_image_load.image
+        image_camera = camera_image_load.image
 
-        doc_descriptor = face_recognition.compute_face_descriptor(image_doc, shape_doc)
-        cam_descriptor = face_recognition.compute_face_descriptor(image_cam, shape_cam)
+        document_descriptor = face_recognition.compute_face_descriptor(image_document, shape_document)
+        camera_descriptor = face_recognition.compute_face_descriptor(image_camera, shape_camera)
 
-        doc_descriptor = np.array(list(doc_descriptor))
-        cam_descriptor = np.array(list(cam_descriptor))
+        document_descriptor = np.array(list(document_descriptor))
+        camera_descriptor = np.array(list(camera_descriptor))
 
-        a = np.linalg.norm(doc_descriptor - cam_descriptor)
+        a = np.linalg.norm(document_descriptor - camera_descriptor)
 
-        if a < 0.60:
+        if a < THRESHOLD:
             result = ':green[Identity has been successfully confirmed!]'
-            bbox_color = (0, 255, 0)
+            bbox_color = GREEN
         else:
             result = ':red[Identity has not been confirmed!]'
-            bbox_color = (255, 0, 0)
+            bbox_color = RED
 
-        marked_face_doc = draw_facemask(image_doc, bbox_doc, shape_doc, bbox_color)
-        marked_face_cam = draw_facemask(image_cam, bbox_cam, shape_cam, bbox_color)
+        marked_face_document = draw_facemask(image_document, bbox_document, shape_document, bbox_color)
+        marked_face_camera = draw_facemask(image_camera, bbox_camera, shape_camera, bbox_color)
 
-        h_doc, w_doc = marked_face_doc.shape[:2]
-        h_cam, w_cam = marked_face_cam.shape[:2]
-        max_side_doc = max(h_doc, w_doc)
-        max_side_cam = max(h_cam, w_cam)
+        height_document, width_document = marked_face_document.shape[:2]
+        height_camera, width_camera = marked_face_camera.shape[:2]
+        max_side_document = max(height_document, width_document)
+        max_side_camera = max(height_camera, width_camera)
 
-        if max_side_cam > max_side_doc:
-            scale = max_side_cam / max_side_doc
-            marked_face_doc = cv2.resize(marked_face_doc, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
+        if max_side_camera > max_side_document:
+            scale = max_side_camera / max_side_document
+            marked_face_document = cv2.resize(marked_face_document, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
 
         else:
-            scale = max_side_doc / max_side_cam
-            marked_face_cam = cv2.resize(marked_face_cam, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
+            scale = max_side_document / max_side_camera
+            marked_face_camera = cv2.resize(marked_face_camera, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
 
-        col_left.image(marked_face_doc, use_column_width=False)
-        col_right.image(marked_face_cam, use_column_width=False)
+        left_column.image(marked_face_document, use_column_width=False)
+        right_column.image(marked_face_camera, use_column_width=False)
 
         st.subheader(result)
